67749646110c2fdd01853f659a8b8b3af94f3b48
anderson-andrade-dev <147995158+anderson-andrade-dev@users.noreply.github.com>
2024-08-07 14:40:19 -0300

    criada classe que formata datas

--- src/main/java/br/com/exp7orer/centauri/controller/MensagemController.java
+++ src/main/java/br/com/exp7orer/centauri/controller/MensagemController.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+
 
 import java.util.List;
 
@@ -36,21 +36,12 @@ public class MensagemController {
             return "redirect:/login";
         }
         mensagemModel.enviar(usuario, mensagem);
-        model.addAttribute("mensagem", "Mensagem enviada com sucesso!");
-        model.addAttribute("usuario", usuario);
-        return "/lista";
-    }
-
-    @PostMapping("/lista")
-    public String mensagensUsuario(Usuario usuario, Model model) {
-        if (usuario.getId() < 1) {
-            return "redirect:/login";
-        }
         List<MensagemSistema> mensagens = mensagemModel.mensagens(usuario);
         model.addAttribute("mensagem", "Mensagem enviada com sucesso!");
-        model.addAttribute("usuario", usuario);
         model.addAttribute("mensagens", mensagens);
+
         return "testes/listaMensagem";
     }
+
 
 }
