82ab8703d222163663fdff60948a3b0b4a23b93b
Anderson Andrade Dev <andersonandradedev@outlook.com>
2024-08-22 13:18:35 -0300

    criação das funções de alterar, excluir, publicação.

--- src/main/java/br/com/exp7orer/centauri/model/LoginModel.java
+++ src/main/java/br/com/exp7orer/centauri/model/LoginModel.java
@@ -3,7 +3,6 @@ package br.com.exp7orer.centauri.model;
 import br.com.exp7orer.centauri.entity.Usuario;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
 
 @Component
 public class LoginModel {
