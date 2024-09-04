5a90ca64afe18021fabe113a46bbe91998eb7f82
Doug <dougZV@protonmail.com>
2024-09-03 21:15:09 -0300

    add script verificacao usuario

--- src/main/java/br/com/exp7orer/centauri/repository/LoginRepository.java
+++ src/main/java/br/com/exp7orer/centauri/repository/LoginRepository.java
@@ -4,4 +4,5 @@ import br.com.exp7orer.centauri.entity.Login;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LoginRepository extends JpaRepository<Login,Long> {
+	boolean existsByNomeUsuario(String nomeUsuario);
 }
