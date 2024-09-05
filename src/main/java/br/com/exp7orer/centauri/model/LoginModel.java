6c5adcba04dafbbfebc894d0f9a79a22f290f303
Anderson Andrade Dev <andersonandradedev@outlook.com>
2024-09-05 15:06:14 -0300

    Correção do bug do commit

--- src/main/java/br/com/exp7orer/centauri/model/LoginModel.java
+++ src/main/java/br/com/exp7orer/centauri/model/LoginModel.java
@@ -1,29 +1,9 @@
 package br.com.exp7orer.centauri.model;
 
-
-import org.springframework.stereotype.Component;
-
-@Component
-public class LoginModel {
-
-import br.com.exp7orer.centauri.entity.Login;
 import br.com.exp7orer.centauri.entity.Usuario;
 import br.com.exp7orer.centauri.repository.LoginRepository;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
-
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Optional;
-import java.util.Set;
 
 @Component
 public class LoginModel {
@@ -31,7 +11,7 @@ public class LoginModel {
     
 
     @Autowired
-    public LoginModel(UsuarioModel usuarioModel,LoginRepository loginRepository) {
+    public LoginModel(UsuarioModel usuarioModel, LoginRepository loginRepository) {
         this.usuarioModel = usuarioModel;
     }
 
